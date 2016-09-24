e6cf05430f95dc3993c92905ec0e037c3991eb3f
Nodari <kielsenodari@gmail.com>
2016-09-24 14:13:09 -0700

    Enemies, gold and spikeballs

---
+++ goldsmash_unity/Assets/_toiletDinasaur/MovingBullet.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingBullet : MonoBehaviour {
+
+	[SerializeField]
+	Rigidbody myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		myRigidbody = GetComponent<Rigidbody>();
+		myRigidbody.velocity.Set( 0,0,1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ goldsmash_unity/Assets/_toiletDinasaur/Shooting.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Shooting : MonoBehaviour {
+
+	[SerializeField]
+	GameObject bullet;
+
+	[SerializeField]
+	GameObject bulletParent;
+
+	float timeToShoot;
+
+	// Use this for initialization
+	void Start () {
+		timeToShoot = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeToShoot > 3.0)
+		{
+			timeToShoot = 0;
+			ShootBullet();
+		}else
+		{
+			timeToShoot += Time.deltaTime;
+		}
+	}
+
+	private void ShootBullet()
+	{
+		print(transform.name + "shoot");
+		GameObject go = Instantiate(bullet, bulletParent.transform) as GameObject;
+		go.transform.position = bulletParent.transform.position;
+		go.transform.rotation = bulletParent.transform.rotation;
+
+	}
+}
